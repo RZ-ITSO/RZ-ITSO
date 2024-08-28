147746d48e13f39e6fded60b5445916699c3408e
MHI <RZ-MD-Github@it-systemsost.de>
2024-08-28 11:39:11 +0200

    Updated the monitoring script, so that it copies only once a day.

--- FolderMonitoring.ps1
+++ FolderMonitoring.ps1
@@ -1,9 +1,14 @@
 # Set the source and destination paths
-$sourceFolder      = "D:\MHI_Basis\Export\archiv\Pforzheim"
-$destinationFolder = "C:\Users\MHI\Documents\Sync-SFTP-FolderCheck"
+$sourceFolder = "D:\MHI_Basis\Export\archiv\Pforzheim"
+$targetFolder = "C:\Users\MHI\Documents\Sync-SFTP-FolderCheck"
 
 # Set the log path
 $LogPath = "C:\RZ-Github\monitoring.log"
+
+
+$LastCopyTime = (Get-Date).AddDays(-1)  # This is set one day behind, so at start the next copy can be created, and variable is still a datetime object.
+$CopyTimeOut  = 86400                   # 1 Day in seconds
+
 
 # Define a logging function
 Function Out-Log ($String){
@@ -17,12 +22,17 @@ while ($true) {
     # Check if the folder exists
     if (Test-Path $sourceFolder) {
         # Get the list of items in the folder
-        $items = Get-ChildItem -Path $sourceFolder
+        $itemsSource = Get-ChildItem -Path $sourceFolder -Recurse
         
-        # Check if the folder is not empty
-        if ($items.Count -gt 0) {
+        <# See, if there is a difference between folders -> now unnecessary, since I check for datetime and timeout
+        $itemsTarget = Get-ChildItem -Path $targetFolder -Recurse
+        $compareFolders = Compare-Object -ReferenceObject $sourceFolder -DifferenceObject $targetFolder #>
+
+        # Check if the folder is not empty and it timeout of 1 day, since last copy has been reached
+        if ($itemsSource.Count -gt 0 -and (New-TimeSpan -Start $LastCopyTime.TotalSeconds -gt ($CopyTimeOut)) ) {
             # Copy the folder recursively to the destination
-            Copy-Item -Path $sourceFolder -Destination $destinationFolder -Recurse -Force
+            Copy-Item -Path $sourceFolder -Destination $targetFolder -Recurse -Force
+            $LastCopyTime = Get-Date
             Out-Log "Folder copied successfully"
         } else {
             Out-Log "Folder exists but is empty"
@@ -31,6 +41,6 @@ while ($true) {
         Out-Log "Folder not found"
     }
 
-    # Wait for 1 minute (60 seconds)
-    Start-Sleep -Seconds 60
+    # Wait for 5 minute (300 seconds)
+    Start-Sleep -Seconds 300
 }
